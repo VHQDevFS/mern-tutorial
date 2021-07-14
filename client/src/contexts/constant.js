d6c2cc4e6c154fb89ed429627a544943de099051
Viet Hoang <vhq.devfs@gmail.com>
2021-07-15 00:30:53 +0700

    fix node env

--- client/src/contexts/constant.js
+++ client/src/contexts/constant.js
@@ -1,13 +1,13 @@
 export const apiUrl =
-  process.env.NODE_ENV !== 'production'
-    ? 'https://vibrant-moliere-84093.herokuapp.com/api'
-    : 'http://localhost:5000/api';
+	process.env.NODE_ENV !== 'production'
+		? 'http://localhost:5000/api'
+		: 'https://vibrant-moliere-84093.herokuapp.com/api'
 
-export const LOCAL_STORAGE_TOKEN_NAME = 'learnit-mern';
+export const LOCAL_STORAGE_TOKEN_NAME = 'learnit-mern'
 
-export const POSTS_LOADED_SUCCESS = 'POSTS_LOADED_SUCCESS';
-export const POSTS_LOADED_FAIL = 'POSTS_LOADED_FAIL';
-export const ADD_POST = 'ADD_POST';
-export const DELETE_POST = 'DELETE_POST';
-export const UPDATE_POST = 'UPDATE_POST';
-export const FIND_POST = 'FIND_POST';
+export const POSTS_LOADED_SUCCESS = 'POSTS_LOADED_SUCCESS'
+export const POSTS_LOADED_FAIL = 'POSTS_LOADED_FAIL'
+export const ADD_POST = 'ADD_POST'
+export const DELETE_POST = 'DELETE_POST'
+export const UPDATE_POST = 'UPDATE_POST'
+export const FIND_POST = 'FIND_POST'
